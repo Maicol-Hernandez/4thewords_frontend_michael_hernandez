686eb11f23ed62329d77199c1e48f4879f0cffb8
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-05 17:13:50 -0500

    Refactors project to use JavaScript
    Removes TypeScript and related configurations to simplify the project setup and reduce complexity.  The project now uses JavaScript for its implementation.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LegendListView from '../views/LegendListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: LegendListView },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
     {
       path: '/about',
       name: 'about',
