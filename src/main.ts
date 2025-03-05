70d6b98f9e45df7897bff704b4ec5a09690a87e8
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-05 11:41:34 -0500

    Adds PrimeVue UI components
    Integrates PrimeVue library for enhanced UI elements.  Adds Button and InputText components for improved user interface.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(PrimeVue, { ripple: true });
+
+app.component('Button', Button);
+app.component('InputText', InputText);
 
 app.use(createPinia())
 app.use(router)
